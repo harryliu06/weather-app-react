1b6ccf43891f1b3e6848b0eae909c7f06ebb9e6a
Harry Liu <127314259+harryliu06@users.noreply.github.com>
2023-09-11 19:40:26 -0700

    delete component

--- src/components/loading_spinner.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function loading_spinner() {
-  return (
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-}
